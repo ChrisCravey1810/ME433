982da094be20fec7eae74037a8f2addaa310096a
ChrisCravey1810 <christopheracravey@gmail.com>
2023-05-04 15:59:18 -0500

    hw8 pixel and outline done

--- HW8/programmer.X/hw8_template.c
+++ HW8/programmer.X/hw8_template.c
@@ -1,76 +1,51 @@
 #include "nu32dip.h" // constants, functions for startup and UART
 #include "i2c_master_noint.h"
-#include "mpu6050.h"
+//#include "mpu6050.h"
 #include <stdio.h>
+#include <string.h>
 #include "font.h"
 #include "ssd1306.h"
 
 void blink(int, int); // blink the LEDs function
+void wait(float);
 
 int main(void) {
     NU32DIP_Startup(); // cache on, interrupts on, LED/button init, UART init
-    init_mpu6050();
-	
-    unsigned char d[14];
-	// char array for the raw data
-	// floats to store the data
-	float ax, ay, az, gx, gy, gz, t;
-	// read whoami
-    unsigned char who;
-    who = whoami();
-	// print whoami
-    char m[100];
-    sprintf(m,"0x%X\r\n", who);
-    NU32DIP_WriteUART1(m);
+    i2c_master_setup();
+    ssd1306_setup();
     
-	 //if whoami is not 0x68, stuck in loop with LEDs on
-    if(who != 0x68){
-        while(1){
-            blink(20, 250);
-        }       
+    ssd1306_drawPixel(0b00001010, 0b00001010, 0b00000001);
+    
+    ssd1306_update();
+    wait(1000);
+    ssd1306_clear();
+    ssd1306_update();
+    blink(10,100);
+    //1 character is 5/8
+    
+    unsigned char m[100];
+    sprintf(m, "hello");
+    drawChar(m[0],x,y);
+	
+   
+}
+
+
+void drawChar(unsigned char letter,unsigned char x, unsigned char y ){
+    for (j=0; j<5; j++){
+        unsigned char col = ASCII[letter -0x20, j];
+        for(i=0; i<8; i++){
+            drawPixel(x+j,y+i, (col>>i) & 0b1);
+        }
     }
     
-    char m_in[100];
-	// wait to print until you get a newline
-    NU32DIP_ReadUART1(m_in,100);
+}
 
-    while (1) {
-		// use core timer for exactly 100Hz loop
-        _CP0_SET_COUNT(0);
-        blink(1, 5);
-
-        // read IMU
-        burst_read_mpu6050(d);
-		// convert data
-        ax = conv_xXL(d);
-        ay = conv_yXL(d);
-        az = conv_zXL(d);
-        gx = conv_xG(d);
-        gy = conv_yG(d);
-        gz = conv_zG(d);
-        t = conv_temp(d);
-        
-        // print out the data
-        sprintf(m,"%f\r\n", ax);
-        NU32DIP_WriteUART1(m);
-//        sprintf(m,"%f\r\n", ay);
-//        NU32DIP_WriteUART1(m);
-//        sprintf(m,"%f\r\n", az);
-//        NU32DIP_WriteUART1(m);
-//        sprintf(m,"%f\r\n", gx);
-//        NU32DIP_WriteUART1(m);
-//        sprintf(m,"%f\r\n", gy);
-//        NU32DIP_WriteUART1(m);
-//        sprintf(m,"%f\r\n", gz);
-//        NU32DIP_WriteUART1(m);
-//        sprintf(m,"%f\r\n", t);
-//        NU32DIP_WriteUART1(m);
-     
-        
-        
-        
-        while (_CP0_GET_COUNT() < 48000000 / 2 / 100) {
-        }
+void drawString(unsigned char *m, unsigned char x, unsigned char y){
+    int k = 0;
+    while(m[k]!=0){
+        drawChar(m[k],x + (5*k),y);
+        k++;
     }
 }
 
@@ -96,3 +71,10 @@ void blink(int iterations, int time_ms) {
     }
 }
 
+
+void wait(float waitMs) {
+    unsigned long t = _CP0_GET_COUNT(); 
+    // the core timer ticks at half the SYSCLK, so 24000000 times per second
+    // so each millisecond is 24000 ticks
+    while(_CP0_GET_COUNT() < t + 24000*waitMs){}
+}
